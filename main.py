74d093e4b86afc4b85e2600bcc338afc59c0bf68
Austin Brehm <austin.brehm@gmail.com>
2023-04-21 14:55:37 -0600

    clean up code, read data from .xlsx file using Pandas

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-# predict if a coffee is over $5 based on size
-
+# Goal: predict if a coffee is over $5 based on size (oz).
+# Logistic regression with stochastic gradient descent is used to
+# find w,b values of a sigmoid function that minimize a cost function.
 import numpy as np
 import matplotlib.pyplot as plt
-from math import e, log
+import pandas as pd
 import math
+from math import e, log
 
 
-# data: x_train = size, y_train = true/false price is over $5
-size = np.array([8, 12, 16, 20, 8, 8, 8, 8, 8, 12, 12, 12, 12, 12])
-price = np.array([0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
-
-
-def plot_data(x, y, title, x_label, y_label):
+def plot(x, y, w, b, title, x_label, y_label):
     plt.style.use('dark_background')
     plt.scatter(x, y, marker='o', c='lime')
-    plt.title(title)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.show()
-
-
-def plot_sigmoid(x, w, b, title, x_label, y_label):
-    plt.style.use('dark_background')
-    plt.plot(x, 1/(1 + e**-(w*x + b)), 'fuchsia')
+    plt.plot(x, 1 / (1 + e ** -(w * x + b)), 'fuchsia')
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
@@ -33,7 +22,7 @@ def compute_cost_function(x, y, w, b):
     m = x.shape[0]
     cost = 0.0
     for i in range(m):
-        y_hat = 1/(1 + e**-(w * x[i] + b))
+        y_hat = 1 / (1 + e ** -(w * x[i] + b))
         cost = y[i] * log(y_hat) + (1 - y[i]) * log(1 - y_hat)
     cost = cost / -m
     return cost
@@ -65,41 +54,48 @@ def gradient_descent(x, y, w, b, alpha, iterations):
             cost_history.append(compute_cost_function(x, y, w, b))
         if i % math.ceil(iterations / 10) == 0:
             print(f'Iteration {i}: Cost {cost_history[-1]:8.2f}')
+
     return w, b, cost_history
 
 
-# initialize w, b
-# Note: change these parameters to alter model output
+# 1. Data Collection [Note: x_train = size, y_train = binary label [true/false price is over $5]]
+data = pd.read_excel('data.xlsx')
+size = np.array(data['size (oz)'])
+price = np.array(data['binary label'])
+
+# 2. Initialize w, b, alpha (learning rate), and iterations [Note: change these parameters to alter model output]
 initial_w = 0
 initial_b = 0
 initial_alpha = 0.000005
 initial_iterations = 10000
 
-# compute initial cost using initial parameters
+# 3. Compute initial cost using initial parameters
 initial_cost = compute_cost_function(size, price, initial_w, initial_b)
 print(f'Initial Cost Function Value: {initial_cost:0.2f}')
 
-# compute gradient
+# 4. Compute gradient
 w_gradient, b_gradient = compute_gradient(size, price, initial_w, initial_b)
 print(f'dj_dw: {w_gradient:0.2f}\ndj_db: {b_gradient:0.2f}')
 
-# compute w and b using gradient descent
+# 5. Compute w and b using gradient descent
 w_final, b_final, cost_history_final = gradient_descent(size, price, initial_w, initial_b, initial_alpha,
                                                         initial_iterations)
 print(f'w_final: {w_final:0.2f}\nb_final: {b_final:0.2f}')
 
+# 6. Compute final cost
 final_cost = compute_cost_function(size, price, w_final, b_final)
 print(f'Final Cost: {final_cost:0.2f}')
 
-# predict if a 10 oz coffee is over $5 using w_final and b_final
-size_10 = 1/(1 + e**-(w_final * 10 + b_final))
+# 7. Predict if a 10 oz coffee is over $5 using w_final and b_final
+size_10 = 1 / (1 + e ** -(w_final * 10 + b_final))
 print(f'Prediction for 10 oz: {size_10:0.2f}')
 
+# 8. Classification threshold
 if size_10 > 0.5:
     print('OVER $5.00')
 else:
     print('UNDER $5.00')
 
-# plot initial data and sigmoid function
-# plot_data(size, price, 'Coffee Prices', 'size (oz)', 'price ($)')
-# plot_sigmoid(size, w_final, b_final, 'Logistic Regression', 'size (Soz)', 'prediction')
+# 9. Plot initial data and sigmoid function
+plot(size, price, w_final, b_final, 'Logistic Regression: Coffee Prices', 'size (oz)',
+     'probability [P(coffee price > $5)] ')
